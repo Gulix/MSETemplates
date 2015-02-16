094cf3a5760d3c706067a2b07614640455e293ca
gulix <gulix33xp@yahoo.fr>
2015-02-16 11:36:20 +0100

    Ajout d'un répertoire permettant de stocker divers fichiers d'aide Ajout d'un batch permettant de créer les mse-installer

---
+++ _misc/batches/Readme.txt
@@ -0,0 +1,4 @@
+Batches utiles pour la maintenance du projet, pour gérer les fichiers, ...
+
+** create-installer.bat
+ - Crée les mse-installer des projets
---
+++ _misc/batches/create-installer.bat
@@ -0,0 +1,6 @@
+set version=1.0.1
+
+mse.exe --create-installer wquest.%version%.mse-installer wquest.mse-game
+mse.exe --create-installer bloodbowl.%version%.mse-installer bloodbowl.mse-game
+mse.exe --create-installer sobah.%version%.mse-installer sobah.mse-game
+mse.exe --create-installer gulixtemplates.%version%.mse-installer wquest.mse-game bloodbowl.mse-game sobah.mse-game
