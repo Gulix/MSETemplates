feaff5760fbd63bfa7a5ce95ef11eed45e4a3fe8
Gulix <gulix33xp@yahoo.fr>
2015-07-23 13:59:16 +0200

    Updating the batch creating the installer New packages from Pulp City were not added to the installer file

--- _misc/batches/create-installer.bat
+++ _misc/batches/create-installer.bat
@@ -1,4 +1,4 @@
-set version=1.0.1
+set version=1.2.4
 set listePckWQ=wquest.mse-game wquest-dungeondeck.mse-style wquest-eventdeck.mse-style wquest-magicitems.mse-style wquest-miscdeck.mse-style wquest-monsterdeck.mse-style
 mse.exe --create-installer wquest.%version%.mse-installer %listePckWQ%
 
@@ -8,7 +8,7 @@ mse.exe --create-installer bloodbowl.%version%.mse-installer %listePckBB%
 set listePckSBH=sobah.mse-game sobah-default.mse-style sobah-magic.mse-game sobah-magic-default.mse-style
 mse.exe --create-installer sobah.%version%.mse-installer %listePckSBH%
 
-set listePckPulp=pulpcity-supreme.mse-game pulpcity-icons.mse-symbol-font pulpcity-supreme-classic.mse-style
+set listePckPulp=pulpcity-icons.mse-symbol-font pulpcity-supreme.mse-game pulpcity-supreme-classic.mse-style pulpcity-supreme-minions.mse-style pulpcity-cards.mse-game pulpcity-cards-agendas.mse-style pulpcity-cards-agp_cards.mse-style pulpcity-cards-factions.mse-style pulpcity-cards-leaders.mse-style pulpcity-cards-plots.mse-style
 mse.exe --create-installer pulpcity.%version%.mse-installer %listePckPulp%
 
 mse.exe --create-installer gulixtemplates.%version%.mse-installer %listePckSBH% %listePckBB% %listePckWQ% %listePckPulp%
