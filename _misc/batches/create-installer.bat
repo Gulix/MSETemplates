4c3d15ae5e5519f1e46209de2d0af37eda0c495c
Gulix <gulix33xp@yahoo.fr>
2016-03-08 19:22:17 +0100

    Updating batch of installer creation

--- _misc/batches/create-installer.bat
+++ _misc/batches/create-installer.bat
@@ -1,4 +1,4 @@
-set version=1.4.0
+set version=1.5.0
 set listePckWQ=wquest.mse-game wquest-dungeondeck.mse-style wquest-eventdeck.mse-style wquest-magicitems.mse-style wquest-miscdeck.mse-style wquest-monsterdeck.mse-style
 mse.exe --create-installer wquest.%version%.mse-installer %listePckWQ%
 
@@ -11,7 +11,7 @@ mse.exe --create-installer sobah.%version%.mse-installer %listePckSBH%
 set listePckPulp=pulpcity-icons.mse-symbol-font pulpcity-supreme.mse-game pulpcity-supreme-classic.mse-style pulpcity-supreme-minions.mse-style pulpcity-cards.mse-game pulpcity-cards-agendas.mse-style pulpcity-cards-agp_cards.mse-style pulpcity-cards-factions.mse-style pulpcity-cards-leaders.mse-style pulpcity-cards-plots.mse-style
 mse.exe --create-installer pulpcity.%version%.mse-installer %listePckPulp%
 
-set listePckFrostgrave=frostgrave-spells.mse-game frostgrave-spells-gulix.mse-style frostgrave-spells-classic.mse-style
+set listePckFrostgrave=frostgrave-spells.mse-game frostgrave-spells-gulix.mse-style frostgrave-spells-classic.mse-style frostgrave-creatures.mse-game frostgrave-creatures-flakes.mse-style
 mse.exe --create-installer frostgrave.%version%.mse-installer %listePckFrostgrave%
 
 set listePckOnePage=onepagerules.mse-game onepagerules-default.mse-style
